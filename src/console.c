e9d3323c32b04cc1284bb9af599b38e8d9582e30
Momoyeyu <momoyeyu@outlook.com>
2024-06-09 11:39:39 +0800

    实现程序自由命名

--- src/console.c
+++ src/console.c
@@ -169,14 +169,14 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
         cmd_dir(cons);
     else if (strncmp(cmdline, "type ", 5) == 0)
         cmd_type(cons, fat, cmdline);
-    else if (strcmp(cmdline, "hlt") == 0)
-        cmd_hlt(cons, fat);
     else if (cmdline[0] != 0)
-    {
-        // 不是命令，也不是空行
-        putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
-        cons_newline(cons);
-        cons_newline(cons);
+    { // 不是命令，也不是空行
+        if (cmd_app(cons, fat, cmdline) == 0)
+        { // 不是命令，不是应用程序，也不是空行
+            putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
+            cons_newline(cons);
+            cons_newline(cons);
+        }
     }
     return;
 }
@@ -270,12 +270,31 @@ void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline)
     return;
 }
 
-void cmd_hlt(struct CONSOLE *cons, int *fat)
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 {
-    struct FILEINFO *finfo = file_search("HLT.HRB", (struct FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+    struct FILEINFO *finfo;
     struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
     struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
-    char s[30], *p;
+    char name[18], *p;
+    int i;
+    for (i = 0; i < 13; i++)
+    {
+        if (cmdline[i] == 0 || cmdline[i] == ' ')
+            break;
+        name[i] = cmdline[i];
+    }
+    name[i] = 0;
+    finfo = file_search(name, (struct FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+    if (finfo == 0 && name[i - 1] != '.')
+    { // 由于找不到文件，故在文件名后面加上“.hrb”后重新寻找
+        name[i] = '.';
+        name[i + 1] = 'H';
+        name[i + 2] = 'R';
+        name[i + 3] = 'B';
+        name[i + 4] = 0;
+        finfo = file_search(name, (struct FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+    }
+
     if (finfo)
     { // 找到文件的情况
         p = (char *)memman_alloc_4k(memman, finfo->size);
@@ -284,11 +303,10 @@ void cmd_hlt(struct CONSOLE *cons, int *fat)
         // farjmp(0, 1003 * 8); // 切换到hlt进程
         farcall(0, 1003 * 8); // 切换到hlt进程
         memman_free_4k(memman, (int)p, finfo->size);
+        return 1;
     }
-    else
-    { // 没有找到文件的情况
-        putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "File not found.", 15);
-        cons_newline(cons);
-    }
-    return;
+    // 没有找到文件的情况
+    // putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "File not found.", 15);
+    // cons_newline(cons);
+    return 0;
 }
